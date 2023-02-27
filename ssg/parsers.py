6fa2d28d8351828dfca63c979e2f9a84e2320fb8
Angel Saiz <a.saiz.v@gmail.com>
2023-02-26 20:34:43 -0500

    Module 4

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -52,6 +52,8 @@ class MarkdownParser(Parser):
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
 
+        hooks.event("written")
+
 
 class ReStructuredTextParser(Parser):
     file_exts = [".rst"]
@@ -64,3 +66,4 @@ class ReStructuredTextParser(Parser):
         sys.stdout.write(
             "\x1b[1;32m{} converted to HTML. Metadata: {}\n".format(path.name, content)
         )
+        hooks.event("written")
